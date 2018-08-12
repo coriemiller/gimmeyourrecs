3be37cf00eecb0164c3e22f611ffb50c83f36a38
Corie Miller <coriem0213@gmail.com>
2018-08-12 14:34:09 -0400

    remove pycache

--- router.py
+++ router.py
@@ -16,8 +16,8 @@ def create_user():
 	redirect_url = 'http://' + username + '.' + app.config['SERVER_NAME']
 	return redirect(redirect_url, code=302)
 
-# @app.route("/", subdomain="<username>")
-# def username_index(username):
-#     """Dynamic subdomains are also supported
-#     Try going to user1.your-domain.tld/dynamic"""
-#     return username + " the subdomain worked"
+@app.route("/", subdomain="<username>")
+def username_index(username):
+    """Dynamic subdomains are also supported
+    Try going to user1.your-domain.tld/dynamic"""
+    return username + " the subdomain worked"
